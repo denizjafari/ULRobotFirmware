ee75cfd5211862eb0c9ffdc551b082a803bfbc43
Alex Pasat <123707273+alex-pasat@users.noreply.github.com>
2024-10-22 12:14:05 -0400

    implemented ble using aioble

--- Haptic_Firmware/Python/main.py
+++ Haptic_Firmware/Python/main.py
@@ -1,4 +1,9 @@
-from machine import Pin, PWM
+from micropython import const
+import aioble
+import bluetooth
+import struct
+import asyncio
+from machine import Pin
 import time
 
 # Setup 
@@ -13,10 +18,44 @@ buzzer_pwm = PWM(buzzer_pin, freq=1000)
 # Set duty cycle to 40%. This is needed for the buzzer as vrated is 1.5V and 
 duty_40_percent = int(0.45*1023)
 
-#Function Definitions
+# UUIDs for BLE service and characteristic
+_SERVICE_UUID = bluetooth.UUID("12345678-1234-5678-1234-56789abcdef0")
+_CHAR_UUID = bluetooth.UUID("12345678-1234-5678-1234-56789abcdef1")
+
+# Create BLE service and characteristic
+service = aioble.Service(_SERVICE_UUID)
+char = aioble.Characteristic(service, _CHAR_UUID, write=True, read=True)
+
+# Register the service
+aioble.register_services(service)
+
+# handle_data: Function to await client connection and handle incoming data
+async def handle_data():
+    while True:
+        try:
+            # Wait for a raspberry pi (client) to connect
+            async with await aioble.advertise(250_000, name="ESP32-C3", services=[_SERVICE_UUID]) as connection:
+                print("Connected to:", connection.device)
+
+                # Wait for data to be written to the characteristic
+                while True:
+                    data = await char.written()
+                    print("Data received:", data)
+
+                    # Call haptic function if called by client
+                    if value == b'\0x01':
+                        print("Run Haptics")
+                        await run_alert()
+                    else:
+                        print("Unknown command")
+
+        except asyncio.CancelledError:
+            print("Connection closed")
+        except Exception as e:
+            print("Error:", e)
 
 #run_alert: Function that is used to alert user
-def run_alert():
+async def run_alert():
     for i in range(5):
         buzzer_pwm.duty(duty_40_percent)
         haptic_pin.on()
@@ -28,6 +67,12 @@ def run_alert():
 
         time.sleep(0.4)
 
-#main
 
-run_alert()
+# Main function to run the BLE task
+async def main():
+    await handle_data()
+
+# The ble connection is run as a asynchronous run to prevent concurent tasks blocking each other, if
+# other asynchronous tasks were added.
+asyncio.run(main())
+
